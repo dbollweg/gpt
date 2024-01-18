d48f4eb6129edaa75f648d66743410d7e3f778a2
dbollweg <bollwegdennis@gmail.com>
2024-01-18 14:22:54 -0500

    add adaptive zeuthen flow test

---
+++ tests/qcd/zeuthen_flow_adaptive.py
@@ -0,0 +1,23 @@
+import gpt as g
+
+
+grid = g.grid([8,8,8,8], g.double)
+
+rng = g.random('test')
+
+U = g.qcd.gauge.random(grid,rng)
+plaq = g.qcd.gauge.stencil.plaquette(U)
+
+g.message("Initial plaquette: ", plaq)
+epsilon = 0.001
+maxtau = 2
+g.message("Starting zeuthen flow with adaptive stepsize initial eps = ", epsilon)
+
+U_flowed = g.qcd.gauge.smear.zeuthen_flow_gauge.zeuthen_flow_gauge_adaptive(U,epsilon,maxTau=maxtau)
+
+plaq = g.qcd.gauge.stencil.plaquette(U_flowed)
+
+g.message(f"Plaquette after zeuthen flow: {plaq}")
+
+assert(abs(plaq-1.0) < 1e-5)
+g.message("Test passed!")
